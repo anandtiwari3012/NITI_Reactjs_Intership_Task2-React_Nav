c7028098410281abbf9e16d15aac9aff20c89b39
anandtiwari3012 <anandtiwari301220@gmail.com>
2024-03-18 13:16:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Navbar = () => {
+    return (
+      <div>
+        <nav className="navbar">
+            <div className="brand">Anand Tiwari</div>
+              <ul className="nav-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/">About</a></li>
+                <li><a href="/">Services</a></li>
+                <li><a href="/">Contact</a></li>
+              </ul>
+              <div className="menu-toggle">&#9776;</div>
+        </nav>
+        <div className='body'>
+          <h1>Hello Welcome To My Page</h1>
+        </div>
+        </div> 
+    );
 }
 
-export default App;
+export default Navbar;
